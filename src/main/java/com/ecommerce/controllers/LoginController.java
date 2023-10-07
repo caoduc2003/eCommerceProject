3ca23cd0e1e570bd387bf87b019a123f7de139e6
caoduc2003 <ducldche176120@fpt.edu.vn>
2023-10-08 00:25:31 +0700

    change logic

--- src/main/java/com/ecommerce/controllers/LoginController.java
+++ src/main/java/com/ecommerce/controllers/LoginController.java
@@ -12,45 +12,50 @@ import jakarta.servlet.http.HttpSession;
 import java.io.IOException;
 import java.sql.SQLException;
 
-@WebServlet(name = "LoginController", urlPatterns = {"/LoginController","/login"})
+@WebServlet(name = "LoginController", urlPatterns = {"/login"})
 public class LoginController extends HttpServlet {
     private static final String INVALID_PAGE = null;
+    private static final String LOGIN_PAGE = "login.jsp";
     private static final String HOME_PAGE = "index.jsp";
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        String url = INVALID_PAGE;
+        String url = LOGIN_PAGE;
+        String button = request.getParameter("btAction");
         String username = request.getParameter("txtEmail");
         String password = request.getParameter("txtPassword");
         try {
-            LoginDAO dao = new LoginDAO();
-            boolean result = dao.checkLogin(username, password);
-            if (result) {
-                url = HOME_PAGE;
-                HttpSession session = request.getSession();
-                session.setAttribute("USERNAME",username);
-            }// End if username and password matched
-
+            if (button == null) {
+                url = LOGIN_PAGE;
+            } else if (button.equals("Login")) {
+                LoginDAO dao = new LoginDAO();
+                boolean result = dao.checkLogin(username, password);
+                if (result) {
+                    url = HOME_PAGE;
+                    HttpSession session = request.getSession();
+                    session.setAttribute("USERNAME", username);
+                } // End if username and password matched
+            }
         } catch (SQLException | ClassNotFoundException ex) {
             ex.printStackTrace();
         } catch (Exception e) {
             throw new RuntimeException(e);
         } finally {
-//            response.sendRedirect(url);
-          RequestDispatcher rd = request.getRequestDispatcher(url);
-          rd.forward(request, response);
+            response.sendRedirect(url);
+//            RequestDispatcher rd = request.getRequestDispatcher(url);
+//            rd.forward(request, response);
         }
 
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        processRequest(req,resp);
+        processRequest(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        processRequest(req,resp);
+        processRequest(req, resp);
     }
 }
