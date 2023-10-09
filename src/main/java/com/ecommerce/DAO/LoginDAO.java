a21324de865de1006cf96c912c641dbfc0381e4f
caoduc2003 <ducldche176120@fpt.edu.vn>
2023-10-09 18:35:32 +0700

    change return datatype from bool to DTO object

--- src/main/java/com/ecommerce/DAO/LoginDAO.java
+++ src/main/java/com/ecommerce/DAO/LoginDAO.java
@@ -6,28 +6,22 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import com.ecommerce.models.*;
-
 import com.ecommerce.utils.DBHelper;
 import com.ecommerce.models.LoginDTO;
 
 public class LoginDAO implements Serializable {
-    private final String INVALID_PAGE = null;
-    private final String HOME_PAGE = "index.jsp";
 
-
-
-    public boolean checkLogin (String username, String password)
-            throws SQLException, ClassNotFoundException{
+    public LoginDTO checkLogin(String username, String password)
+            throws SQLException, ClassNotFoundException {
         Connection con = null;
         PreparedStatement stm = null;
         ResultSet rs = null;
-        try{
+        try {
             //1. Connect DB
             con = DBHelper.makeConnection();
-            if(con!=null){
+            if (con != null) {
                 //2. Create SQL String
-                String sql="SELECT * FROM Users WHERE email = ? AND password = ? ";
+                String sql = "SELECT * FROM Users WHERE email = ? AND password = ? ";
                 //3. Create Statement
                 stm = con.prepareStatement(sql);
                 stm.setString(1, username);
@@ -35,22 +29,25 @@ public class LoginDAO implements Serializable {
                 //4. Excute Query
                 rs = stm.executeQuery();
                 //5. Process Result
-                if(rs.next() && rs.getString("account_status").equals("active")){
-                    return true;
+                if (rs.next() && rs.getString("account_status").equals("active")) {
+                    String firstName = rs.getString("first_name");
+                    String lastName = rs.getString("last_name");
+                    String email = rs.getString("email");
+                    String role = rs.getString("role");
+                    return new LoginDTO(firstName, lastName, username, email, password, role);
                 }
             }
-        }
-        finally{
-            if(rs != null){
+        } finally {
+            if (rs != null) {
                 rs.close();
             }
-            if(stm != null){
+            if (stm != null) {
                 stm.close();
             }
-            if(con != null){
+            if (con != null) {
                 con.close();
             }
         }
-        return false;
+        return null;
     }
 }
